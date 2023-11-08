102d853722d2a4e820e630a4581324e802edc59b
LouiseV98 <143037181+LouiseV98@users.noreply.github.com>
2023-11-08 17:09:29 -0600

    FInal
    Cambios al codigo y un pequeño detalle

--- Practica 9.py
+++ Practica 9.py
@@ -2,67 +2,90 @@ import threading
 import time
 import tkinter as tk
 
-archivo_sem = threading.Semaphore(1)
-archivo_contenido = ""
-texto_editar = "Texto a editar en tiempo real."
+archivo_Semaforo = threading.Semaphore(1)
+edicion_Semaforo = threading.Semaphore(1)
+contenido_Archivo = ""
+texto_Editar = "Lorem ipsum dolor sit amet, consectetur adipisicing elitin, sed do eiusmod tempor tur adipisicing elitin, sed do eiusmod tempor tur adipisicing elitin, sed do eiusmod tempor"
 
-def leer_archivo(ventana_id, text_widget):
-    global archivo_contenido
-    text_widget.delete('1.0', 'end')
+ventanas_Editando = []
+ventanas_Leyendo = 0
+
+def leer_archivo(ventana_Id, texto_Ventana):
+    global contenido_Archivo, ventanas_Leyendo
+    archivo_Semaforo.acquire()
+    ventanas_Leyendo += 1
+    if ventanas_Leyendo == 1:
+        edicion_Semaforo.acquire()  # Bloquear edición si es la primera ventana en leer
+    archivo_Semaforo.release()
+    texto_Ventana.delete('1.0', 'end')
     lectura_concurrente()
-    for caracter in archivo_contenido:
-        text_widget.insert('end', caracter)
-        text_widget.update()
-        time.sleep(0.1)  # Simula un pequeño retraso por carácter
+    for caracter in contenido_Archivo:
+        texto_Ventana.insert('end', caracter)
+        texto_Ventana.update()
+        time.sleep(0.1)
+    archivo_Semaforo.acquire()
+    ventanas_Leyendo -= 1
+    if ventanas_Leyendo == 0:
+        edicion_Semaforo.release()  # Desbloquear edición si no hay ventanas leyendo
+    archivo_Semaforo.release()
 
-def simular_escritura(ventana_id, text_widget):
-    global archivo_contenido
-    for caracter in texto_editar:
-        archivo_sem.acquire()
-        nuevo_contenido = archivo_contenido + caracter
-        text_widget.delete('1.0', 'end')
-        text_widget.insert('end', nuevo_contenido)
-        text_widget.update()
-        archivo_contenido = nuevo_contenido
-        archivo_sem.release()
-        time.sleep(0.1)  # Simula un pequeño retraso por carácter
+def simular_escritura(ventana_Id, texto_Ventana):
+    global contenido_Archivo
+    archivo_Semaforo.acquire()
+    edicion_Semaforo.acquire()
+    ventanas_Editando.append(ventana_Id)  # Agregar ventana a la lista de edición
+    for caracter in texto_Editar:
+        nuevo_Contenido = contenido_Archivo + caracter
+        texto_Ventana.delete('1.0', 'end')
+        texto_Ventana.insert('end', nuevo_Contenido)
+        texto_Ventana.update()
+        contenido_Archivo = nuevo_Contenido
+        time.sleep(0.1)
+    ventanas_Editando.remove(ventana_Id)  # Eliminar ventana de la lista de edición
+    edicion_Semaforo.release()
+    archivo_Semaforo.release()
 
-def guardar_archivo(ventana_id, text_widget):
-    global archivo_contenido
-    archivo_sem.acquire()
-    text_widget.delete('1.0', 'end')
-    text_widget.insert('end', "Guardando...")
-    text_widget.update()
-    time.sleep(2)  # Simula un proceso de guardado de 2 segundos
-    text_widget.delete('1.0', 'end')
-    text_widget.insert('end', "Guardado completado.")
-    text_widget.update()
-    archivo_sem.release()
+def guardar_archivo(ventana_Id, texto_Ventana):
+    global contenido_Archivo
+    archivo_Semaforo.acquire()
+    edicion_Semaforo.acquire()
+    while len(ventanas_Editando) > 0 or ventanas_Leyendo > 0:  # Esperar a que las ventanas de edición y lectura terminen
+        time.sleep(0.1)
+    texto_Ventana.delete('1.0', 'end')
+    texto_Ventana.insert('end', "Guardando...")
+    texto_Ventana.update()
+    time.sleep(2)
+    texto_Ventana.delete('1.0', 'end')
+    texto_Ventana.insert('end', "Guardado completado.")
+    texto_Ventana.update()
+    edicion_Semaforo.release()
+    archivo_Semaforo.release()
 
 def lectura_concurrente():
-    while archivo_sem._value == 0:  # Esperar a que se libere el archivo
+    while archivo_Semaforo._value == 0:
         time.sleep(0.1)
 
 def ventana(ventana_id):
     root = tk.Tk()
     root.title(f"Ventana {ventana_id}")
-    text_widget = tk.Text(root, wrap=tk.WORD)
-    text_widget.pack()
+    texto_Ventana = tk.Text(root, wrap=tk.WORD)
+    texto_Ventana.pack()
 
-    leer_button = tk.Button(root, text="Leer", command=lambda: leer_archivo(ventana_id, text_widget))
-    editar_button = tk.Button(root, text="Editar")
-    guardar_button = tk.Button(root, text="Guardar", command=lambda: guardar_archivo(ventana_id, text_widget))
+    boton_Leer = tk.Button(root, text="Leer", state=tk.NORMAL)
+    boton_Editar = tk.Button(root, text="Editar")
+    boton_Guardar = tk.Button(root, text="Guardar")
 
-    leer_button.pack()
-    editar_button.pack()
-    guardar_button.pack()
+    boton_Leer.pack()
+    boton_Editar.pack()
+    boton_Guardar.pack()
 
     # Agregar evento para iniciar la simulación de escritura
-    editar_button.config(command=lambda: simular_escritura(ventana_id, text_widget))
-
+    boton_Editar.config(command=lambda: simular_escritura(ventana_id, texto_Ventana))
+    boton_Guardar.config(command=lambda: guardar_archivo(ventana_id, texto_Ventana))
+    boton_Leer.config(command=lambda: leer_archivo(ventana_id, texto_Ventana))
+    
     root.mainloop()
 
-# Ejecutar las tres ventanas en hilos separados
 thread1 = threading.Thread(target=ventana, args=(1,))
 thread2 = threading.Thread(target=ventana, args=(2,))
 thread3 = threading.Thread(target=ventana, args=(3,))
